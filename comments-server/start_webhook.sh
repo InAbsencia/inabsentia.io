09a58c0a270bf69a11f5aa3a1ab156385aa3a40c
iyyel <iyyel@protonmail.com>
2018-08-24 10:38:22 +0000

    Update repo.

--- comments-server/start_webhook.sh
+++ comments-server/start_webhook.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo webhook -hooks test.json -verbose -secure -cert /etc/letsencrypt/live/iyyel.io/fullchain.pem -key /etc/letsencrypt/live/iyyel.io/privkey.pem &
+sudo webhook -hooks hooks.json -verbose -secure -cert /etc/letsencrypt/live/iyyel.io/fullchain.pem -key /etc/letsencrypt/live/iyyel.io/privkey.pem &
