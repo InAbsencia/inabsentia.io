8edda14a27b600f909c1a879c50a3b99e6bb462f
iyyel <iyyel@protonmail.com>
2019-04-18 18:22:09 +0000

    Updated maintenance page.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 # iyyel.io deployment script
+echo "Deployment started..."
+
+echo "Building..."
 bundle exec jekyll build
+echo "Done."
+
+echo "Copying new files..."
 sudo rm -rf /var/www/iyyel.io/public_html/*
 sudo cp -r html/* /var/www/iyyel.io/public_html/.
+echo "Done."
+
+echo "Cleaning up..."
 sudo rm -rf .sass-cache html Gemfile.lock
+echo "Done."
+
 echo 'Deployment finished.'
