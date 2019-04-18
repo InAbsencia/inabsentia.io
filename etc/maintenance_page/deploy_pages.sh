cfce9acbdd74e3b155877160c32b2191588b8b45
iyyel <iyyel@protonmail.com>
2019-04-18 15:54:24 +0000

    Created maintenance page script.

--- etc/maintenance_page/deploy_pages.sh
+++ etc/maintenance_page/deploy_pages.sh
@@ -15,8 +15,11 @@ fi
 
 echo "Executing..."
 
-echo "Cleaning directories.."
+echo "Building..."
+bundle exec jekyll build
+echo "Done."
 
+echo "Cleaning directories.."
 sudo rm -rf /var/www/ai.iyyel.io/public_html/*
 sudo rm -rf /var/www/alpha.iyyel.io/public_html/*
 sudo rm -rf /var/www/beta.iyyel.io/public_html/*
@@ -32,13 +35,28 @@ sudo rm -rf /var/www/sys.iyyel.io/public_html/*
 sudo rm -rf /var/www/test.iyyel.io/public_html/*
 sudo rm -rf /var/www/tmp.iyyel.io/public_html/*
 sudo rm -rf /var/www/usr.iyyel.io/public_html/*
-
 echo "Done."
 
 echo "Copying files..."
+sudo cp -r html/* /var/www/ai.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/alpha.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/beta.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/bin.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/code.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/data.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/dat.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/dev.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/img.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/ml.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/srv.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/sys.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/test.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/tmp.iyyel.io/public_html/.
+sudo cp -r html/* /var/www/usr.iyyel.io/public_html/.
+echo "Done."
 
-
-
+echo "Cleaning up..."
+sudo rm -rf .sass-cache html Gemfile.lock
 echo "Done."
 
 echo "Script done."
